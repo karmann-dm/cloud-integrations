d4ea75881d8c3df5d00d7f7214be3870b933e95c
karmann-dm <karmanno@gmail.com>
2019-11-03 20:57:16 +0300

    Initial

---
+++ src/main/java/ru/karmanno/cloudsintegration/CloudsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package ru.karmanno.cloudsintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudsIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudsIntegrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/karmanno/cloudsintegration/CloudsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.karmanno.cloudsintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudsIntegrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
